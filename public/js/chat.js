4465328987b2a017437813daef3873c9a06d7f80
Shivam Saraf <shivamasaraf10@gmail.com>
2020-06-03 22:16:10 -0400

    Made some changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,13 +2,31 @@ const socket = io()
 document.querySelector('#initial-one').addEventListener('submit', (e) =>{
     e.preventDefault()
     
-    const name = e.target.elements.message.value
+    const name = document.getElementById('myname').value
     socket.emit('receivename', name)
 })
 
 
 socket.on('sendmessage', (message) => {
     console.log(message)
+})
+
+
+document.querySelector('#Location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        console.log("This feature ain't supported for you :(")
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('getlocation',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+
+        // Let us also change the inner html of the html.
+        var location = document.getElementById("mylocation").innerHTML = `Your position right now will be ${position.coords.latitude} latitudes and 
+        ${position.coords.longitude} longitudes.`
+    })
 })
 
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 
+// Connecting to a database.
+
+const mongodb = require('mongodb')
+
+// This will give us the neccessary functions to be able to connect to our client efficiently.
+const MongoClient = mongodb.MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'my-chat-app'
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if(error){
+        console.log('Unable to connect man.')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('userinfo').insertOne({
+        name: 'Shivam',
+        email: 'shivamasaraf@yahoo.com'
+    })
+    console.log('connected!')
+})
+
+
+
+
 
 const app = express()
 const server = http.createServer(app)
@@ -14,7 +42,7 @@ const port = process.env.PORT || 3000
 const publicdirectorypath = path.join(__dirname, '../public')
 app.use(express.static(publicdirectorypath))
 
-
+var mongoose = require('mongoose')
 
 io.on('connection', (socket) => {
     
@@ -23,13 +51,19 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('sendmessage', 'A user has left.')
+        socket.broadcast.emit('sendmessage','A user has left.')
+    })
+
+    socket.on('getlocation', (position) => {
+        socket.emit('sendmessage', `http://www.google.com/maps/place/${position.latitude},${position.longitude}`)
     })
 
 
     socket.broadcast.emit('sendmessage', 'A new user has joined people!')
 
 })
+
+
 
 server.listen(port, () => {
     console.log('Server is up!!!')
